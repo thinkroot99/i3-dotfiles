4e267dfde772e050441ad0bb4cd4b20b9f2eaa9d
ThinkRoot99 <social@crism.ro>
2023-09-01 22:10:15 +0300

    Create lib.pl

---
+++ i3/cmus/lib.pl
@@ -0,0 +1,118 @@
+/home/cristian/Music/audio/2Pac - Changes (Official Music Video) ft. Talent-eXvBjCO19QY.m4a
+/home/cristian/Music/audio/4 nopti si 4 zile (patru nopti si patru zile) - manele vechi.mp3
+/home/cristian/Music/audio/Ace of Base - Beautiful Life (Official Music Video)-wh-07BzfgYY.m4a
+/home/cristian/Music/audio/Adriana Rusu feat Tataee  - Noi doi.mp3
+/home/cristian/Music/audio/Adrian Minune - Asa Sunt Zilele Mele ( Muzica Originala ).mp3
+/home/cristian/Music/audio/Alexandru Pop - Dumnezeu, pă om, când bate.mp3
+/home/cristian/Music/audio/Alexandru Pop - Mândrele-atâta-s curate.mp3
+/home/cristian/Music/audio/Alexandru Pop - Mi-o dat Dumnezeu un gând.mp3
+/home/cristian/Music/audio/Alex de la Orastie-Pe gropita Official video 4K.mp3
+/home/cristian/Music/audio/Alex de la Orastie si Valentin Dinu - Asta-i soarta omului-3hPYKFRInnE.m4a
+/home/cristian/Music/audio/Alexia - Pentru Tine _ Official Video.mp3
+/home/cristian/Music/audio/Alexia - Pentru Tine _ Piano Session.mp3
+/home/cristian/Music/audio/Amedeo Minghi - Edera.mp3
+/home/cristian/Music/audio/AMI feat. Tata Vlad - Enigma _ Official Video.mp3
+/home/cristian/Music/audio/AMI - Fluturi _ Official Music Video.mp3
+/home/cristian/Music/audio/Andia - Mi-e dor _ Official Video.mp3
+/home/cristian/Music/audio/Andia x Spike - Anotimpuri _ Official Video.mp3
+/home/cristian/Music/audio/Andrii Popa - videoclip oficial 1975.mp3
+/home/cristian/Music/audio/An Theos - Noaptea Sânzienelor (Official Video).mp3
+/home/cristian/Music/audio/Bomfunk MC's - Freestyler (Video Original Version)-ymNFyxvIdaM.m4a
+/home/cristian/Music/audio/B.U.G. Mafia - O Lume Nebuna, Nebuna De Tot (feat. ViLLy) (Prod. Tata Vlad) (Videoclip).mp3
+/home/cristian/Music/audio/B.U.G. Mafia - Poveste Fara Sfarsit (feat. Catalina) (Prod. Tata Vlad).mp3
+/home/cristian/Music/audio/B.U.G. Mafia - Strazile feat. Mario V (Prod. Tata Vlad) (Videoclip)-AG56t8t9iW8.m4a
+/home/cristian/Music/audio/Cargo - Daca ploaia s-ar opri (Official Video).mp3
+/home/cristian/Music/audio/Cargo - Nu ma lasa sa-mi fie dor (Official Video).mp3
+/home/cristian/Music/audio/Casanova - Mama ce m-am imbatat.mp3
+/home/cristian/Music/audio/@Cipri Popescu⚡️@Ghiţă Munteanu Official💥 Cu un strop de dragoste [videoclip oficial].mp3
+/home/cristian/Music/audio/Cipri Popescu ❤️ O viață și încă o zi [videoclip oficial].mp3
+/home/cristian/Music/audio/Cipri Popescu ❤️ Te iubesc 💍 (dansul mirilor).mp3
+/home/cristian/Music/audio/Cocos de la Calarasi - Hai sa spargem gheata [official video] 4K.mp3
+/home/cristian/Music/audio/Cocos de la Calarasi - Sete de iubire _ Official Video.mp3
+/home/cristian/Music/audio/Codu' Penal cu Don Baxter si Nico - Toata noaptea (2001-2021).mp3
+/home/cristian/Music/audio/Codu' Penal - Singur Pe Strada (feat. Nico) (Videoclip).mp3
+/home/cristian/Music/audio/Coolio - Gangsta's Paradise (feat. L.V.) [Official Music Video]-fPO76Jlnz6c.m4a
+/home/cristian/Music/audio/Costi ❎ Cream ❎ Minodora ❎ Diana Bucsa - ROMANIA MEA 🇲🇩-JBkkXTSQO7w.m4a
+/home/cristian/Music/audio/Costi ❎ Cream ❎ Minodora ❎ Diana Bucsa - ROMANIA MEA 🇲🇩.mp3
+/home/cristian/Music/audio/Costi Ioniță & Alina - Necazuri și supărări ( Oficial Video ).mp3
+/home/cristian/Music/audio/Costi Ioniță & Florin Salam - Doar cu tine ( Oficial Video ).mp3
+/home/cristian/Music/audio/Cred Că Sunt Extraterestru - Nu Pot Să Mă Schimb (cu Ugly Boy).mp3
+/home/cristian/Music/audio/CRISTIAN RIZESCU, DENISA & PLAY AJ - Vino, Vino - HIT.mp3
+/home/cristian/Music/audio/Cryss Style ✘ Zeno Music - Cate fete sunt pe lume _ Remix-D3XRJSaXooA.m4a
+/home/cristian/Music/audio/Cryss Style ✘ Zeno Music - Cate fete sunt pe lume _ Remix.mp3
+/home/cristian/Music/audio/DENISA & FLORIN PESTE - Daca pozele ar vorbi - HIT.mp3
+/home/cristian/Music/audio/Denis Spaniolu'❌Nu-i vina la mine❌2021█▬█ █ ▀█▀.mp3
+/home/cristian/Music/audio/Denis Spaniolu’ - Vreau sa-ti dau iubire _ Ovidiu Lupu TikTok Remix ⚡ Zeno Music.mp3
+/home/cristian/Music/audio/DJ Project feat. Andia - Slabiciuni _ Official Video.mp3
+/home/cristian/Music/audio/DJ Project x MIRA - Cheia Inimii Mele _ Official Video.mp3
+/home/cristian/Music/audio/Dr. Alban - It's My Life-4zHm_6AQ7CY.m4a
+/home/cristian/Music/audio/Eminem - Love The Way You Lie ft. Rihanna-uelHwf8o7_U.m4a
+/home/cristian/Music/audio/Eminem - Mockingbird (Official Music Video)-S9bCLPwzSC0.m4a
+/home/cristian/Music/audio/Eminem - Not Afraid (Official Video)-j5-yKhDd64s.m4a
+/home/cristian/Music/audio/Europe - The Final Countdown (Official Video)-9jK-NcRmVcw.m4a
+/home/cristian/Music/audio/Fara Zahar - D'la sate.mp3
+/home/cristian/Music/audio/F.Charm - A fost odată (Videoclip Oficial).mp3
+/home/cristian/Music/audio/F.Charm - Dacă m-aș naște din nou feat. @HOYNAR , @TECSAN , Mihai Meiros (Videoclip Oficial).mp3
+/home/cristian/Music/audio/F.Charm - Mi-e dor (Videoclip Oficial).mp3
+/home/cristian/Music/audio/Florinel cu Ioana si Play AJ - Te iubesc oriunde ai fi.mp3
+/home/cristian/Music/audio/Florin Peste❌Aleena - Cine suntem noi (Official Video).mp3
+/home/cristian/Music/audio/Georgiana Lobont ❌ Adrian Peste - Nu mai dau la nimenea _ Florin Peste Records.mp3
+/home/cristian/Music/audio/Georgiana Lobont - Palincuta margelata (Official Video).mp3
+/home/cristian/Music/audio/Georgiana Lobont - Tu ma faci sa plang de dor (Clip Oficial 2021).mp3
+/home/cristian/Music/audio/Getto Daci - Marea Scofală [1999].mp3
+/home/cristian/Music/audio/Getto Daci - Nu Ma Poti Da Jos [1999].mp3
+/home/cristian/Music/audio/GLOBAL Top Songs of 2021 _ 1 HOUR MUSIC MIX-Z-6QOLgzMcI.m4a
+/home/cristian/Music/audio/Gojira & Planet H feat. B.U.G. Mafia - #MMDJ (Prod. Planet H).mp3
+/home/cristian/Music/audio/HARA & TAMANGO   Muro Shavo.mp3
+/home/cristian/Music/audio/Ioana Ignat & Edward Sanda - Daca tu nu ai fi (Official Video).mp3
+/home/cristian/Music/audio/Ioana Ștefan - Hai la joc bădiță hăi.mp3
+/home/cristian/Music/audio/Ioana Ștefan  -  Îs băciță de la munte.mp3
+/home/cristian/Music/audio/Ionica Morosanu - Io si cu Vasile-a nost _ Videoclip Oficial.mp3
+/home/cristian/Music/audio/KAIRA - Armele Mele (Official Video).mp3
+/home/cristian/Music/audio/Keri & Butch feat. Omar Corbu - Fericirea.mp3
+/home/cristian/Music/audio/Keri & Butch - Nu semănăm.mp3
+/home/cristian/Music/audio/Laura si Alex de la Orastie - Tu meriti mandruto draga.mp3
+/home/cristian/Music/audio/Lena Miclaus si Alex de la Orastie  - Da-mi dragostea ta.mp3
+/home/cristian/Music/audio/Lidia Buble - Camasa (Official Video).mp3
+/home/cristian/Music/audio/Meat Loaf - It's All Coming Back to Me Now-B6IPajggiVM.m4a
+/home/cristian/Music/audio/Meat Loaf I Would Do Anything For Love Lyrics-srEe6K34DVo.m4a
+/home/cristian/Music/audio/Menuett - Colo-n vale (Cantece de munte) HD.mp3
+/home/cristian/Music/audio/Metallica - Nothing Else Matters (Official Music Video)-tAGnKpE4NCI.m4a
+/home/cristian/Music/audio/MIRA - Cheia Inimii Mele (by DJ Project) _ Official Live Session.mp3
+/home/cristian/Music/audio/MIRA x @Mario Fresh - Deziubeste-ma _ Official Video.mp3
+/home/cristian/Music/audio/Nicolae Guta - Cand o pun, o pun cu sacu'.mp3
+/home/cristian/Music/audio/Nicolae Guta  - De-ar fi sa intorc pamantul (oficial video) 2018.mp3
+/home/cristian/Music/audio/Nicolae Guta - E scris undeva.mp3
+/home/cristian/Music/audio/NICOLAE GUTA SI PLAY AJ - BEAU,BEAU (SPIROS GALATI).mp3
+/home/cristian/Music/audio/NICOLAE GUTA SI PLAY AJ - TU ESTI FATA DE ORAS (SPIROS GALATI).mp3
+/home/cristian/Music/audio/NICOLAE GUTA SI SUSANU - PUSTOAICO CU PAR ROSCAT (SPIROS GALATI).mp3
+/home/cristian/Music/audio/Nicolae Guta - Vin la tine puisor.mp3
+/home/cristian/Music/audio/NOCTURNA - New Evil (Official Video)-wGvEr4vtOK0.m4a
+/home/cristian/Music/audio/Oana Font - Dulce-i drumu’ cătră casă.mp3
+/home/cristian/Music/audio/Old Town Road (The Voice Australia 2020 Performance _ Live).mp3
+/home/cristian/Music/audio/Phoenix - Fata verde (High sound quality).mp3
+/home/cristian/Music/audio/Poe - În Lanțuri (Official Video).mp3
+/home/cristian/Music/audio/Proconsul - Un om mai bun (Official Video).mp3
+/home/cristian/Music/audio/Puya cu Anastasia - Maine E O Noua Zi.mp3
+/home/cristian/Music/audio/Raluka - Îndrăgostiți Neuniform  _ Official Music Video.mp3
+/home/cristian/Music/audio/Ramona David - Cu tine as pleca la drum (Oficial Video).mp3
+/home/cristian/Music/audio/ReMan ❌ Tabba - Batalioane-WZMzTkHr7vM.m4a
+/home/cristian/Music/audio/Roxen - Spune-mi _ Official Video.mp3
+/home/cristian/Music/audio/Sa-Mi Canti Cobzar.mp3
+/home/cristian/Music/audio/Scorpions - Wind Of Change (Official Music Video)-n4RjJKxsamQ.m4a
+/home/cristian/Music/audio/Sonia Ratiu - Hai sa spargem gheata (cover).mp3
+/home/cristian/Music/audio/Speak - Sa nu renunti _ Official Video.mp3
+/home/cristian/Music/audio/Spike - Paparude (Videoclip Oficial).mp3
+/home/cristian/Music/audio/Surorile Osoianu și LUPII lui Calancea - La Portiță la Gheorghița_Iubește, Gheorghe, iubește.mp3
+/home/cristian/Music/audio/Surorile Osoianu - Trece-un nouraș pe sus.mp3
+/home/cristian/Music/audio/The Chemical Brothers - Galvanize (Official Music Video)-Xu3FTEmN-eg.m4a
+/home/cristian/Music/audio/The Motans - A mea _ Official Music Video-00Oefe2d3PI.m4a
+/home/cristian/Music/audio/The Motans - A mea--pRSTfs8Kbo.m4a
+/home/cristian/Music/audio/TRANSYLVANIA - Muntii nostri aur poarta.mp3
+/home/cristian/Music/audio/Una Favela-UzUsuHfVb3U.m4a
+/home/cristian/Music/audio/Unde-s haiducii.mp3
+/home/cristian/Music/audio/Valeriu Sterian -- Amintire cu haiduci.mp3
+/home/cristian/Music/audio/VALI VIJELIE - Nu suntem la fel (VIDEO OFICIAL 2015).mp3
+/home/cristian/Music/audio/Vita de Vie - Basul si cu Toba Mare - Videoclip OFICIAL.mp3
+/home/cristian/Music/audio/Zdob și Zdub & Frații Advahov - 'Trenulețul' (official video).mp3
+/home/cristian/Music/audio/Zeno Music ✘ Zolotova Vero - RA TA TA ⚡ Tik Tok Remix.mp3
